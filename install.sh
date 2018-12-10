4d0cddb62b326c6539eb7f06f95849a8136b95f5
Nishanth <nishanthkc009@gmail.com>
2018-12-10 07:13:33 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+conda create --name drlnd python=3.6
+source activate drlnd
+git clone https://github.com/udacity/deep-reinforcement-learning.git
+cd deep-reinforcement-learning/python
+pip install .
+python -m ipykernel install --user --name drlnd --display-name "drlnd"
+git clone https://github.com/Nishanth009/Udacity-Nano-Degree-RL-Project-Navigation
+cd Udacity-Reinforcement-learning-Project---Continous-Control
+wget "https://s3-us-west-1.amazonaws.com/udacity-drlnd/P2/Reacher/Reacher_Linux.zip"
+unzip Reacher_Linux.zip
+wget "https://s3-us-west-1.amazonaws.com/udacity-drlnd/P2/Reacher/Reacher.app.zip"
+unzip Reacher.app.zip
+wget "https://s3-us-west-1.amazonaws.com/udacity-drlnd/P2/Reacher/Reacher_Windows_x86.zip"
+unzip Reacher_Windows_x86.zip
+wget "https://s3-us-west-1.amazonaws.com/udacity-drlnd/P2/Reacher/Reacher_Windows_x86_64.zip"
+unzip Reacher_Windows_x86_64.zip
